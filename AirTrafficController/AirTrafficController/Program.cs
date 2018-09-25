ef32aa4a93f12ede2abae6f253bbffe9448f0e48
au573585 <201611672@post.au.dk>
2018-09-25 10:02:55 +0200

    first edit

---
+++ AirTrafficController/AirTrafficController/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+namespace AirTrafficController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                var list = new List<string>();
+
+                var factory = TransponderReceiverFactory.CreateTransponderDataReceiver();
+                var raw = new RawTransponderDataEventArgs(list);
+            factory.TransponderDataRea
+            while (true)
+            {
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.WriteLine("Press key");
+            Console.ReadKey();
+        }
+    }
+}
