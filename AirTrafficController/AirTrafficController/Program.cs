ffd81f6ed73ae96a8acfdb72559f016209889f61
fabervae <mesebii@hotamil.com>
2018-09-25 11:08:15 +0200

    implemented hookup method. Prints data of every aircraft in the airspace

--- AirTrafficController/AirTrafficController/Program.cs
+++ AirTrafficController/AirTrafficController/Program.cs
@@ -11,22 +11,37 @@ namespace AirTrafficController
     {
         static void Main(string[] args)
         {
-           
-                var list = new List<string>();
+            // Initiate a virtual airspace with air crafts and return a transponder receiver interface to it.
+            var transReceiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+            // Subscribe to transponder events with a custom callback method.
+            transReceiver.TransponderDataReady += TransReceiverOnTransponderDataReady;
 
-                var factory = TransponderReceiverFactory.CreateTransponderDataReceiver();
-                var raw = new RawTransponderDataEventArgs(list);
-            factory.TransponderDataRea
             while (true)
             {
-                foreach (var item in list)
-                {
-                    Console.WriteLine(item);
-                }
+                System.Threading.Thread.Sleep(1000); // Ugly way of saving some cycles
             }
 
             Console.WriteLine("Press key");
             Console.ReadKey();
         }
+
+        private static void TransReceiverOnTransponderDataReady(object sender, RawTransponderDataEventArgs e)
+        {
+            /* TODO remember to check whether the (new) list contains data which has already been printed before.
+             Clearing the list does NOT work as it auto-generates everything within the current airspace.
+             */
+
+            // We iterate over every aircraft.
+            foreach (string airCraftsData in e.TransponderData)
+            {
+                // For each aircraft, split the string into separate data items. 
+                string[] arrayOfAircraftDataItems = airCraftsData.Split(';');
+                Console.WriteLine("Track tag: " + arrayOfAircraftDataItems[0]);
+                Console.WriteLine($"(X,Y) position: {arrayOfAircraftDataItems[1]},{arrayOfAircraftDataItems[2]})");
+                Console.WriteLine("Altitude: " + arrayOfAircraftDataItems[3]);
+                Console.WriteLine("Timestamp: " + arrayOfAircraftDataItems[4]);
+            }
+        }
     }
 }
+    
