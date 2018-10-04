61a5e4f4c985acc2daeaaeab5504232580ebeb4e
au573585 <201611672@post.au.dk>
2018-10-04 11:40:56 +0200

    added testclasses

--- AirTrafficController/AirTrafficController.Test.Unit/AirTrafficControllerTestUnits.cs
+++ AirTrafficController/AirTrafficController.Test.Unit/AirTrafficControllerTestUnits.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AirTrafficController.Framework;
-using AirTrafficController.Test.Unit.Stubs;
+using NSubstitute;
+
+
 using NUnit.Framework;
 using TransponderReceiver;
+
 
 namespace AirTrafficController.Test.Unit
 {
@@ -17,14 +20,18 @@ namespace AirTrafficController.Test.Unit
         private static readonly IDecoder _decoder = new Decoder();
         private static readonly ILogger _logger = new Logger();
         private static readonly ISeparationHandler _separationHandler = new SeparationHandler();
-        private static readonly ITransponderReceiver _transponderReceiver = new StubTransponderReceiver();
         private static readonly ITrack _track = new Track(_separationHandler);
+        private ITransponderReceiver _fakeTransponderReceiver;
+
+        
 
         [SetUp]
         public void Setup()
         {
             // TODO When using integration test, uncomment the following line
             //tC = new TrafficController(_decoder, _logger, _track, _transponderReceiver);
+
+            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
         }
 
         [TestCase(10000,10000,500)]
@@ -44,6 +51,21 @@ namespace AirTrafficController.Test.Unit
         public static void WithInBoundary_XoutOfRange_ResultIsFalse(int x, int y, int a)
         {
             Assert.That(_track.CheckIfWithinBoundary(x,y,a), Is.False);
+        }
+
+        public void TestReception()
+        {
+            List<String> testData = new List<string>();
+            testData.Add("AAA111;630094;83421;12500;20181002204132530");
+            testData.Add("BBB111;930094;3421;3000;20181002204132520");
+            testData.Add("C1C1C1;9094;43210;500;201810022047872530");
+
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+
+            _decoder.DecodeData(testData[0]);
+
+
         }
     }
 }
