57c6ecf21d0fe324714bb5397b8d3a1917fa82cb
au572628 <201611674@post.au.dk>
2018-10-04 18:08:32 +0200

    testlogger file

---
+++ AirTrafficController/AirTrafficController.Test.Unit/TestLogger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficController.Framework;
+using NUnit.Framework;
+using TransponderReceiver;
+
+namespace AirTrafficController.Test.Unit
+{
+    class TestLogger
+    {
+        private ILogger _uut;
+        
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Logger();
+        }
+
+        [Test]
+        public void LoggerOutput_TestIfPrintWasSucces()
+        {
+            string[] printData = new string[] { "ABC123", "15000", "15000", "10000", "20151006213456789" };
+            //_uut.LogData(printData);
+
+            //test if print (Logger) was succes
+            Assert.IsTrue(_uut.LogData(printData));
+        }
+
+        [Test]
+        public void LoggerOutput_TestIfPrintReturnsFalseWithNoData()
+        {
+            string[] printData = new string[] { };
+            //_uut.LogData(printData);
+
+            //test if print (Logger) was succes
+            Assert.IsFalse(_uut.LogData(printData));
+        }
+    }
+}
