77dff64c976e846810fc71f9df661dca6c20983b
Whizel <viktor.soendergaard@hotmail.com>
2018-10-04 11:52:44 +0200

    Fixing IDecodertestUnits

--- AirTrafficController/AirTrafficController.Test.Unit/IDecoderTestUnits.cs
+++ AirTrafficController/AirTrafficController.Test.Unit/IDecoderTestUnits.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirTrafficController.Framework;
 using NUnit.Framework;
 
 namespace AirTrafficController.Test.Unit
