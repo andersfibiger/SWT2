b13b38c556c0b5ebbf3fd627d162f651a7129115
au572628 <201611674@post.au.dk>
2018-10-04 17:49:51 +0200

    Done test for Logger

--- AirTrafficController/AirTrafficController/Logger.cs
+++ AirTrafficController/AirTrafficController/Logger.cs
@@ -5,13 +5,24 @@ namespace AirTrafficController
 {
     public class Logger : ILogger
     {
-        public void LogData(string[] dataTracks)
+        public Boolean LogData(string[] dataTracks)
         {
+            if (dataTracks == null || dataTracks.Length == 0)
+            {
+                //return false if dataTracks is empty
+                return false;
+            }
+
+            else
+            Console.WriteLine("Hej");
             Console.WriteLine("Track tag: " + dataTracks[0]);
             Console.WriteLine($"(X,Y) position: {dataTracks[1]},{dataTracks[2]}");
             Console.WriteLine("Altitude: " + dataTracks[3]);
             Console.WriteLine("Timestamp: " + dataTracks[4]);
             Console.WriteLine("");
+
+            //return true if print succes
+            return true;
         }
     }
 }
