96237abe949ea17c8c94d492e779c5aad110e533
au573585 <201611672@post.au.dk>
2018-10-05 11:09:47 +0200

    test for transponder

---
+++ AirTrafficController/AirTrafficController.Test.Unit/TransponderReceiverTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficController.Framework;
+using NSubstitute;
+
+
+using NUnit.Framework;
+using TransponderReceiver;
+
+
+namespace AirTrafficController.Test.Unit
+{
+    public class TransponderReceiverTest
+    {
+        private TrafficController _uut;
+
+        //this is the thing we actually want to test
+        private ITransponderReceiver _fakeTransponderReceiver;
+        
+        //these are for integrationtest, but will not be tested for now
+        private IDecoder _fakeDecoder;
+        private ILogger _fakeLogger;
+        private ITrack _fakeTrack;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            //make the fakes
+            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            _fakeDecoder = Substitute.For<IDecoder>();
+            _fakeLogger = Substitute.For<ILogger>();
+            _fakeTrack = Substitute.For<ITrack>();
+
+            //inject the fakes in uut
+            _uut = new TrafficController(_fakeDecoder, _fakeLogger, _fakeTrack, _fakeTransponderReceiver);
+        }
+
+       
+        [Test]
+        public void _fakeTransponderReceiver_3Tracks()
+        {
+            List<String> testData = new List<string>();
+            //testData.Add("AAA111;630094;83421;12500;20181002204132530");
+            //testData.Add("BBB111;930094;3421;3000;20181002204132520");
+            //testData.Add("C1C1C1;9094;43210;500;201810022047872530");
+
+            testData.Add("c1c1c1;20000;20000;1000;201810022047872530");
+
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+
+            
+            // TODO Why doesnt it work...
+            //Assert.That(_uut.numberOfTracks,Is.EqualTo(0));
+        }
+        
+    }
+}
