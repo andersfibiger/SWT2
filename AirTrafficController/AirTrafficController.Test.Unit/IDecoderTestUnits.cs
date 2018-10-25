503c2e8a8f1362d6bfd04ccddc3d3c8ea80b6ce0
Whizel <viktor.soendergaard@hotmail.com>
2018-10-25 10:21:37 +0200

    Ingen anelse om hvad jeg har ændret

--- AirTrafficController/AirTrafficController.Test.Unit/IDecoderTestUnits.cs
+++ AirTrafficController/AirTrafficController.Test.Unit/IDecoderTestUnits.cs
@@ -19,25 +19,16 @@ namespace AirTrafficController.Test.Unit
             _uut = new Decoder();
         }
 
-        private static readonly List<string[]> ResultData1 = new List<string[]>()
-        {
-            new string[] {"ATR423", "39045", "12932", "14000", "20151006213456789"},
-            //new string[] { "BCD123", "10005", "85890", "12000", "20151006213456789"},
-            //new string[] {"TESTBOUNDX","5000","0","100","2018"},
-        };
         [Test]
-        public void CheckIf_TestData_HasCountEqualTo_isTrue()
+        public void CheckIf_TestData_HasCountEqualTo_IsTrue()
         {
             //Hardcoreded testData
             List<string> testData = new List<string>();
             testData.Add("ATR423;39045;12932;14000;20151006213456789");
-            //testData.Add("BCD123;10005;85890;12000;20151006213456789");
-            //testData.Add("XYZ987;25059;75654;4000;20151006213456789");
             //Make new RawTransponderDataEventArgs
             RawTransponderDataEventArgs RawtestData = new RawTransponderDataEventArgs(testData);
             //Assert that the decoder 
             List<string[]> DecodedData = _uut.DecodeData(RawtestData);
-            Assert.That(DecodedData, Is.Not.Empty); // Test that the decoder has decoding something
             Assert.That(DecodedData, Has.Count.EqualTo(1)); // Test that the decode only has decoded the one string array
             Assert.That(DecodedData[0].Count, Is.EqualTo(5)); // Test that the string array has the correct amount decoded.
         }
@@ -47,8 +38,6 @@ namespace AirTrafficController.Test.Unit
             //Hardcore testData
             List<string> testData = new List<string>();
             testData.Add("ATR423;39045;12932;14000;20151006213456789");
-            //testData.Add("BCD123;10005;85890;12000;20151006213456789");
-            //testData.Add("XYZ987;25059;75654;4000;20151006213456789");
             // Make new RawTransponderDataEventArgs
             RawTransponderDataEventArgs RawtestData = new RawTransponderDataEventArgs(testData);
             //Hardcorde the correct decoded data
@@ -58,12 +47,10 @@ namespace AirTrafficController.Test.Unit
             // test the data in a for each
             for (int i = 0; i < DecodedData[0].Length; i++)
             {
-                Console.WriteLine("Counting DecodeData...");
                 string data = (string)DecodedData[0][i];
                 //data = CorrectTestData[i];
                 Assert.That(data, Is.EqualTo(CorrectTestData[i]));
             }
-            //Assert.That())
         }
 
     }
