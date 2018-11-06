e8f87b342cae3c5f991bc3ecc13074177ae584ac
Whizel <viktor.soendergaard@hotmail.com>
2018-11-07 00:14:47 +0100

    Added CalculateVelocity class with functionality

---
+++ AirTrafficController/AirTrafficController/CalculateVelocity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficController
+{
+    class CalculateVelocity
+    {
+        public void CalcVelocity(TrackData OldData, TrackData NewData)
+        {
+            double XYDiff = Math.Sqrt((Math.Pow((NewData.X - OldData.X), 2) + Math.Pow((NewData.Y - OldData.Y), 2)));
+            double AltitudeDiff = Math.Abs(OldData.Altitude - NewData.Altitude);
+            double DistanceMoved = Math.Sqrt((Math.Pow(XYDiff, 2) + Math.Pow(AltitudeDiff, 2)));
+            double TimeUsed = NewData.TimeStamp.Subtract(OldData.TimeStamp).TotalSeconds;
+            NewData.Velocity = (DistanceMoved / TimeUsed);
+        }
+    }
+}
