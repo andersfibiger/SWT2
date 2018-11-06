3ee5693e538960720c70802714d10e5b4a5a25cc
fabervae <mesebii@hotmail.com>
2018-11-06 14:08:08 +0100

    Encapsulate data from transponder in a class

---
+++ AirTrafficController/AirTrafficController/TrackData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AirTrafficController
+{
+    public class TrackData
+    {
+        public string TagId { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Altitude { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
