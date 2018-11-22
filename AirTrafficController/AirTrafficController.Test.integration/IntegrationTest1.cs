ed82adb207a3bcf211f924b69eb089783f88bf9f
Whizel <viktor.soendergaard@hotmail.com>
2018-11-22 14:00:48 +0100

    Added new integration test

--- AirTrafficController/AirTrafficController.Test.integration/IntegrationTest1.cs
+++ AirTrafficController/AirTrafficController.Test.integration/IntegrationTest1.cs
@@ -11,17 +11,26 @@ namespace AirTrafficController.Test.integration
 {
     public class IntegrationTest1
     {
+        //
         private ISeparationHandler _separationHandler;
         private CalculateCompassCourse _calculateCompassCource;
         private IDecoder _fakeDecoder;
         private CalculateVelocity _calcVelocity;
         private TrackHandler _sut;
 
+        private ICollection<TrackData> Tracksaver;
+        //Data to use in test
         private TrackData _track1;
         private TrackData _track2;
+        private TrackData _track3;
+        private TrackData _CourseTrack;
         private TrackData _conflictingTrack;
+        private TrackData _conflictingTrack2;
         private List<TrackData> _tracklist1;
         private List<TrackData> _tracklist2;
+        private List<TrackData> _tracklist3;
+
+
 
         [SetUp]
         public void Setup()
@@ -34,6 +43,7 @@ namespace AirTrafficController.Test.integration
             _sut = new TrackHandler(_separationHandler,_calcVelocity,_calculateCompassCource,_fakeDecoder);
             _tracklist1 = new List<TrackData>();
             _tracklist2 = new List<TrackData>();
+            _tracklist3 = new List<TrackData>();
 
 
             _track1 = new TrackData
@@ -53,14 +63,38 @@ namespace AirTrafficController.Test.integration
                 Altitude = 1000,
                 TimeStamp = new DateTime(2018, 04, 05, 20, 20, 20)
             };
+            _track3 = new TrackData
+            {
+                TagId = "ABCDE",
+                X = 90000,
+                Y = 60000,
+                Altitude = 1000,
+                TimeStamp = new DateTime(2018, 04, 05, 20, 20, 22)
+            };
+            _CourseTrack = new TrackData
+            {
+                TagId = "ABCDE",
+                X = 80000,
+                Y = 60000,
+                Altitude = 1000,
+                TimeStamp = new DateTime(2018, 04, 05, 20, 20, 22)
+            };
 
             _conflictingTrack = new TrackData
             {
-                TagId = "GFHIJ",
-                X = 90000,
-                Y = 88000,
-                Altitude = 1100,
+                TagId = "AAAAA",
+                X = 9000,
+                Y = 9000,
+                Altitude = 100,
                 TimeStamp = new DateTime(2018, 04, 05, 20, 20, 18)
+            };
+            _conflictingTrack = new TrackData
+            {
+                TagId = "AAAAB",
+                X = 8900,
+                Y = 8900,
+                Altitude = 99,
+                TimeStamp = new DateTime(2018, 04, 05, 20, 20, 19)
             };
         }
 
@@ -70,29 +104,124 @@ namespace AirTrafficController.Test.integration
         {
             _tracklist1.Add(_track1);
             _tracklist2.Add(_track2);
-            //save old trackdata
-            _sut._oldTracksInBoundary = _tracklist1;
-            
-            _sut.CalculateVelocityAndCompassCourse(_tracklist2);
-            Assert.That(Math.Round(_track2.Velocity), Is.EqualTo(5000));
-        }
 
+            _sut.TrackHandlerDataHandler += (sender, datas) => { Tracksaver = datas; };
+
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist1);
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist2);
+
+            // _sut.CalculateVelocityAndCompassCourse(_tracklist1, _tracklist2);
+            Assert.That(Math.Round(Tracksaver.ElementAt(0).Velocity), Is.EqualTo(5000));
+
+        }
+        [Test]
+        public void TestCalculateVelocity_VelocityIs10000()
+        {
+            _tracklist1.Add(_track1);
+            _tracklist2.Add(_track2);
+            _tracklist3.Add(_track3);
+
+            _sut.TrackHandlerDataHandler += (sender, datas) => { Tracksaver = datas; };
+
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist1);
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist2);
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist3);
+
+            Assert.That(Math.Round(Tracksaver.ElementAt(0).Velocity), Is.EqualTo(10000));
+        }
+        [Test]
+        public void TestCalculateVelocity_VelocityIs0()
+        {
+            _tracklist1.Add(_track1);
+
+            _sut.TrackHandlerDataHandler += (sender, datas) => { Tracksaver = datas; };
+
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist1);
+
+            // _sut.CalculateVelocityAndCompassCourse(_tracklist1, _tracklist2);
+            Assert.That(Math.Round(Tracksaver.ElementAt(0).Velocity), Is.EqualTo(0));
+        }
         [Test]
         public void TestCalculateCompassCourse_compassIs90()
         {
             _tracklist1.Add(_track1);
             _tracklist2.Add(_track2);
-            //save old trackdata
-            _sut._oldTracksInBoundary = _tracklist1;
 
-            _sut.CalculateVelocityAndCompassCourse(_tracklist2);
-            Assert.That(Math.Round(_track2.CompassCourse),Is.EqualTo(90));
+            _sut.TrackHandlerDataHandler += (sender, datas) => { Tracksaver = datas; };
+
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist1);
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist2);
+
+            Assert.That(Tracksaver.ElementAt(0).CompassCourse, Is.EqualTo(90));
+        }
+        [Test]
+        public void TestCalculateCompassCourse_compassIs63()
+        {
+            _tracklist1.Add(_track1);
+            _tracklist2.Add(_track2);
+            _tracklist3.Add(_CourseTrack);
+
+            _sut.TrackHandlerDataHandler += (sender, datas) => { Tracksaver = datas; };
+
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist1);
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist2);
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist3);
+
+            Assert.That(Math.Round(Tracksaver.ElementAt(0).CompassCourse), Is.EqualTo(63));
+        }
+        [Test]
+        public void TestCalculateCompassCourse_compassIs0()
+        {
+            _tracklist1.Add(_track1);
+
+            _sut.TrackHandlerDataHandler += (sender, datas) => { Tracksaver = datas; };
+
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist1);
+
+            Assert.That(Tracksaver.ElementAt(0).CompassCourse, Is.EqualTo(0));
         }
 
         [Test]
         public void TestSeparationHandler_()
         {
 
+        }
+
+        [Test]
+        public void TestLoggerRecievedList()
+        {
+            _tracklist1.Add(_track1);
+
+            _sut.TrackHandlerDataHandler += (sender, datas) => { Tracksaver = datas; };
+
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist1);
+
+            Assert.That(Tracksaver, Is.EqualTo(_tracklist1));
+        }
+
+        [Test]
+        public void TestLoggerRecievedNothing()
+        {
+            
+            _sut.TrackHandlerDataHandler += (sender, datas) => { Tracksaver = datas; };
+
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist1);
+
+            Assert.That(Tracksaver, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void TestLoggerRecievedLastList()
+        {
+            _tracklist1.Add(_track1);
+            _tracklist2.Add(_track2);
+
+            _sut.TrackHandlerDataHandler += (sender, datas) => { Tracksaver = datas; };
+
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist1);
+            _fakeDecoder.DecodedDataHandler += Raise.Event<EventHandler<List<TrackData>>>(this, _tracklist2);
+
+            Assert.That(Tracksaver, Is.EqualTo(_tracklist2));
         }
     }
 }
